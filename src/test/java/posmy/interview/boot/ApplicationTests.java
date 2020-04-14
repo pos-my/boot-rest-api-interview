b8df11519113b8dfa808bb3b770cae630b04914e
Rashidi Zin <rashidi@zin.my>
2020-04-14 14:27:57 +0800

    Initial commit

---
+++ src/test/java/posmy/interview/boot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package posmy.interview.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
