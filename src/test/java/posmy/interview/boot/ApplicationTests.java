b747dd3f5503df672ba84e7359a2dc8bab1042a3
Rashidi Zin <rashidi@zin.my>
2021-01-29 09:56:37 +0800

    Polish default test setup

--- src/test/java/posmy/interview/boot/ApplicationTests.java
+++ src/test/java/posmy/interview/boot/ApplicationTests.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -15,7 +15,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class ApplicationTests {
 
     @Autowired
