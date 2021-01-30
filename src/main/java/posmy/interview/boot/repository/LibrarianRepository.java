cdef78d3efc775baaf9be5ad2c5a86beb867f0ee
Douglas <douglaslim95@gmail.com>
2021-01-30 21:30:18 +0800

    Remove unnecessary models/service/repository

--- src/main/java/posmy/interview/boot/repository/LibrarianRepository.java
+++
@@ -1,9 +0,0 @@
-package posmy.interview.boot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import posmy.interview.boot.entity.LibrarianEntity;
-
-@Repository
-public interface LibrarianRepository extends JpaRepository<LibrarianEntity, Long> {
-}
--- src/main/java/posmy/interview/boot/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package posmy.interview.boot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import posmy.interview.boot.entity.MemberEntity;
-
-@Repository
-public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-}
