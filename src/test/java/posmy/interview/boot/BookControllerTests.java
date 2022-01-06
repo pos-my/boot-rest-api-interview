25f369431f6931106efbc6747ef0699e3d04beb4
Aaron Liew <aaron_liew1818@hotmail.com>
2022-01-06 09:54:03 +0800

    [REFACTOR] Add dirties context in class level

--- src/test/java/posmy/interview/boot/BookControllerTests.java
+++ src/test/java/posmy/interview/boot/BookControllerTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.context.WebApplicationContext;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 class BookControllerTests {
 
     @Autowired
