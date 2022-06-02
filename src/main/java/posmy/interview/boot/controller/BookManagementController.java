753f8cbaaefda0de01fee78934c40a1f6067980d
zulkhairi <zulkhairi@mocaz.com>
2022-06-03 00:29:42 +0800

    initial

--- src/main/java/posmy/interview/boot/controller/BookManagementController.java
+++ src/main/java/posmy/interview/boot/controller/BookManagementController.java
@@ -1,4 +1,7 @@
 package posmy.interview.boot.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BookManagementController {
 }
--- src/main/java/posmy/interview/boot/model/Books.java
+++ src/main/java/posmy/interview/boot/model/Books.java
@@ -2,6 +2,9 @@ package posmy.interview.boot.model;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+
 @Data
+@Entity
 public class Books {
 }
