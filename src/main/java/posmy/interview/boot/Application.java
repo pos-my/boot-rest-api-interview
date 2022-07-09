a198a9bdeabaf17da84c48175a39365e2c979bee
Wen Huan <wenhuan98@hotmail.com>
2022-07-10 00:09:41 +0800

    [ADD] Add some changes in spring boot application

--- src/main/java/posmy/interview/boot/Application.java
+++ src/main/java/posmy/interview/boot/Application.java
@@ -2,8 +2,15 @@ package posmy.interview.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import posmy.interview.boot.respository.UserDetailRepository;
+
+
+@ComponentScan
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserDetailRepository.class)
 public class Application {
 
     public static void main(String[] args) {
